adb83833e12b9904d630ec7920d6aee65fb7a143
Emmanuel Ochoa <eorivera95@gmail.com>
2021-09-20 15:40:01 -0600

    Add RestaurantScreen and RestaurantList

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import RestaurantScreen from './components/RestaurantScreen';
 
 const App = () => {
-  return <div>Hello, world.</div>;
+  return (
+    <div>
+      <RestaurantScreen />
+    </div>
+  );
 };
 
 export default App;
